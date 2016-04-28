eb2c2051b0efedbce70612f8d77c82b36d1303dd
Alasdair Bruce <alasdair@groupon.com>
2016-04-28 02:03:17 -0700

    First cut

---
+++ src/test/java/foo/TestFoo.java
@@ -0,0 +1,41 @@
+package foo;
+
+import com.groupon.jtier.daas.postgres.PostgresRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestFoo {
+
+    @Rule
+    public PostgresRule postgres = PostgresRule.withNewDatabase();
+
+    interface IFoo<T> {
+        int getOne();
+        List<T> getStuff(int n);
+    }
+
+    static class IntList extends ArrayList<Integer> { }
+
+    interface IBar extends IFoo<Integer> {
+        @Override
+        IntList getStuff(int n);
+    }
+
+    public static abstract class Foo implements IBar {
+        @SqlQuery("SELECT 1")
+        public abstract int getOne();
+    }
+
+
+    @Test
+    public void testFoo() {
+        DBI dbi = new DBI(postgres.getTransactionDataSource());
+        dbi.onDemand(Foo.class);
+    }
+}
